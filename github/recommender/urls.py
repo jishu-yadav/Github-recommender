4ae153d95ad1e980fe1262171ec6579b9098b4e3
Priyanka7011 <pk3201134@gmail.com>
2021-12-18 01:13:34 +0530

    static part of repo recommendation

--- github/recommender/urls.py
+++ github/recommender/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     
-    path('',views.index,name='homepage')
+    path('',views.index,name='homepage'),
+    path('repo',views.repo_res,name='repo_res')
 ]
--- github/recommender/views.py
+++ github/recommender/views.py
@@ -4,3 +4,7 @@ from django.http import HttpRequest,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def repo_res(request):
+    return render(request,'repo_rec.html')
+
