8a5696911969527e4b0de3778564df62ac104aa6
Priyanka7011 <pk3201134@gmail.com>
2021-12-19 01:31:29 +0530

    user recommender

--- github/recommender/urls.py
+++ github/recommender/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     
     path('',views.index,name='homepage'),
-    path('repo',views.repo_res,name='repo_res')
+    path('repo',views.repo_rec,name='repo_res'),
+    path('user',views.user_rec,name='repo_rec')
 ]
--- github/recommender/views.py
+++ github/recommender/views.py
@@ -5,6 +5,8 @@ from django.http import HttpRequest,HttpResponse
 def index(request):
     return render(request,'index.html')
 
-def repo_res(request):
+def repo_rec(request):
     return render(request,'repo_rec.html')
 
+def user_rec(request):
+    return render(request,'user_rec.html')
