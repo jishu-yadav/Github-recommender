2d206be38ead527ae1385297dead8a41580af480
Priyanka7011 <pk3201134@gmail.com>
2021-12-18 19:21:42 +0530

    model

--- github/recommender/models.py
+++ github/recommender/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from separatedvaluesfield.models import SeparatedValuesField
 # Create your models here.
+class Repository(models.Model):
+    full_name=models.CharField(max_length=500,blank=True)
+    owner_name=models.CharField(max_length=500,blank=True)
+    repo_url=models.URLField(max_length=500,blank=True)
+    homepage=models.URLField(max_length=500,null=True,blank=True)
+    created_at=models.DateTimeField(blank=True,null=True)
+    watchers=models.IntegerField(default=0)
+    open_issues=models.IntegerField(default=0)
+    forks_count=models.IntegerField(default=0)
+    stargazers_count=models.IntegerField(default=0)
+    content=SeparatedValuesField(max_length=800,token=',',blank=True)
+    
+
+
