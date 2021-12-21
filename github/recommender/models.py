01bb858f299143049b6430429c90656721b83e37
Priyanka7011 <pk3201134@gmail.com>
2021-12-22 01:08:40 +0530

    polishing website and user recommender

--- github/recommender/models.py
+++ github/recommender/models.py
@@ -20,3 +20,13 @@ class GithubRepos(models.Model):
     content=models.TextField(max_length=1000,blank=True)
 
 
+class GithubUsers(models.Model):
+    login=models.CharField(max_length=500,blank=True)
+    avatar_url=models.URLField(max_length=500,blank=True)
+    url=models.URLField(max_length=500,blank=True)
+    git_url=models.URLField(max_length=500,blank=True)
+    top5_repos=models.TextField(max_length=2000,blank=True)
+    name=models.CharField(max_length=500,blank=True)
+    total_repos=models.IntegerField(default=0)
+    all_repos=models.TextField(max_length=5000,blank=True)
+    orgs=models.TextField(max_length=5000,blank=True)
